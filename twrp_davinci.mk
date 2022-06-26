4b0caac2c7d89047ccf14052734e8f743b7cc0cb
AndreyDenikin1 <andrey.denikin8@gmail.com>
2022-06-27 00:25:34 +0300

    new fox_11.0

---
+++ twrp_davinci.mk
@@ -0,0 +1,53 @@
+#
+# Copyright (C) 2019 The TwrpBuilder Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Release name
+PRODUCT_RELEASE_NAME := davinci
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := davinci
+PRODUCT_NAME := twrp_davinci
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Xiaomi Mi 9T
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
+    ro.product.device \
+    ro.product.name \
+    ro.build.product \
+    ro.bootimage.build.date.utc \
+    ro.build.date.utc
+
+# Qcom Decryption
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+
+# Recovery
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libion \
+    libxml2 \
+    libicuuc
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libicuuc.so
